6ea042941032bd632e4ddd96c51d392be7386f97
Janelle Yan <janelle.yan@mail.utoronto.ca>
2024-07-30 14:49:32 -0400

    Added the title and buttons to the Start Screen

--- src/main/userinterface/StartScreenUI.java
+++ src/main/userinterface/StartScreenUI.java
@@ -1,11 +1,40 @@
 package main.userinterface;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
 
 public class StartScreenUI extends JFrame {
+    private JLabel titleLabel = new JLabel("Trivia Game");
 
+    private JButton startButton = new JButton("Start");
+    private JButton settingsButton = new JButton("Settings");
+    private JButton exitButton = new JButton("Exit");
 
     public StartScreenUI() {
+        setTitle("Trivia Quiz");
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        JPanel titlePanel = new JPanel();
+        titleLabel.setFont(new Font("Serif", Font.PLAIN, 50));
+        titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        titlePanel.add(titleLabel);
+
+        add(titlePanel, BorderLayout.NORTH);
+
+        JPanel optionsPanel = new JPanel(new GridLayout(0, 1));
+        startButton.setPreferredSize(new Dimension(400, 50));
+        optionsPanel.add(startButton);
+        settingsButton.setPreferredSize(new Dimension(400, 50));
+        optionsPanel.add(settingsButton);
+        exitButton.setPreferredSize(new Dimension(400, 50));
+        optionsPanel.add(exitButton);
+
+        add(optionsPanel, BorderLayout.SOUTH);
+
+        setVisible(true);
 
     }
 
@@ -14,6 +43,6 @@ public class StartScreenUI extends JFrame {
      * Displays a new Start Screen UI
      */
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(MainMenuUI::new);
+        SwingUtilities.invokeLater(StartScreenUI::new);
     }
 }
