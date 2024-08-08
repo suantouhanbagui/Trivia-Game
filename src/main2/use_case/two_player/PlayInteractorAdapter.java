a58f35e59ac455e3c92ff535f1453558788cf1ba
shenxi90 <xian.shen@mail.utoronto.ca>
2024-08-08 14:06:24 -0400

    update two player's files so that it follows LSP

--- src/main2/use_case/two_player/PlayInteractorAdapter.java
+++ src/main2/use_case/two_player/PlayInteractorAdapter.java
@@ -2,6 +2,7 @@ package main2.use_case.two_player;
 
 import main2.use_case.play.PlayInputBoundary;
 import main2.use_case.play.PlayInputData;
+import main2.use_case.play.OnePlayerInteractor;
 import main2.use_case.play.PlayInteractor;
 import main2.use_case.settings.SettingsDTO;
 import main2.use_case.settings.SettingsInteractor;
@@ -10,12 +11,12 @@ import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
 public class PlayInteractorAdapter implements PlayInputBoundary, PropertyChangeListener {
-    private final PlayInteractor playInteractor;
+    private final OnePlayerInteractor playInteractor;
     private final TwoPlayerInteractor twoPlayerInteractor;
-
     private PlayInteractor activeInteractor;
 
-    public PlayInteractorAdapter(PlayInteractor playInteractor,
+
+    public PlayInteractorAdapter(OnePlayerInteractor playInteractor,
                                  TwoPlayerInteractor twoPlayerInteractor,
                                  SettingsInteractor settingsInteractor) {
         this.playInteractor = playInteractor;
