8c3a21e26c56b033fa49830e59d81729d933e6a3
Janelle Yan <janelle.yan@mail.utoronto.ca>
2024-08-08 00:19:44 -0400

    Implemented music

--- src/main2/Main2.java
+++ src/main2/Main2.java
@@ -1,7 +1,15 @@
 package main2;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.IOException;
+import java.net.URL;
+
 public class Main2 {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        AdditionalFeatureMusic.playSound("\\z_additional_features\\adventure_game_music.wav");
     }
 }
