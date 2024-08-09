1eae636c353b6ed54cd0357f242c94f826a21e4a
Leo Xu <138391019+LeoXuIsAvailable@users.noreply.github.com>
2024-08-09 00:37:17 -0400

    Revert "update two players's files so it follows LSP"

--- src/main2/use_case/two_player/PlayInteractorAdapter.java
+++ src/main2/use_case/two_player/PlayInteractorAdapter.java
@@ -2,7 +2,6 @@ package main2.use_case.two_player;
 
 import main2.use_case.play.PlayInputBoundary;
 import main2.use_case.play.PlayInputData;
-import main2.use_case.play.OnePlayerInteractor;
 import main2.use_case.play.PlayInteractor;
 import main2.use_case.settings.SettingsDTO;
 import main2.use_case.settings.SettingsInteractor;
@@ -11,12 +10,12 @@ import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
 public class PlayInteractorAdapter implements PlayInputBoundary, PropertyChangeListener {
-    private final OnePlayerInteractor playInteractor;
+    private final PlayInteractor playInteractor;
     private final TwoPlayerInteractor twoPlayerInteractor;
+
     private PlayInteractor activeInteractor;
 
-
-    public PlayInteractorAdapter(OnePlayerInteractor playInteractor,
+    public PlayInteractorAdapter(PlayInteractor playInteractor,
                                  TwoPlayerInteractor twoPlayerInteractor,
                                  SettingsInteractor settingsInteractor) {
         this.playInteractor = playInteractor;
